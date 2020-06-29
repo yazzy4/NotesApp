5e5b70c8887d97a494c138ed7f10cc56b2c3d183
yazzy4 <yazburrell@pursuit.org>
2020-06-29 17:05:25 -0400

    Added core data model and files

---
+++ Note+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Note+CoreDataClass.swift
+//  NotesApp
+//
+//  Created by Yaz Burrell on 6/29/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Note: NSManagedObject {
+
+}
---
+++ Note+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Note+CoreDataProperties.swift
+//  NotesApp
+//
+//  Created by Yaz Burrell on 6/29/20.
+//  Copyright © 2020 Yaz Burrell. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Note {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
+        return NSFetchRequest<Note>(entityName: "Note")
+    }
+
+    @NSManaged public var body: String?
+
+}
